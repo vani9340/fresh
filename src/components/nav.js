a04a627e5c1f59ab804cb1081f91da0276ff6608
vani9340 <sg5428702@gmail.com>
2023-05-17 20:16:12 +0530

    c

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,7 @@ import Dropdown from './dropdown'
 function Nav(){
     return(
 <BrowserRouter>
-    <div class='w-screen min-h-[1115px] bg-gradient-to-b from-[#FAE3B6] flex'>
+    <div class='w-screen min-h-[1115px] bg-gradient-to-b from-[#FAE3B6] flex max-sm:pt-[1.5rem]  pt-[2.8125rem]'>
         <div class='font-semibold text-[1.5rem]'>
         Fresh
         </div>
@@ -23,12 +23,12 @@ function Nav(){
           <Dropdown/>  
         </div>
         
-        <button className="font-semibold text-[0.8rem]    
-        w-[1px] h-[25px]  bg-gradient-to-r   from-[#FDBB57] from-[-60%] 
+        <button className="max-sm:text-[0.9rem] max-sm:w-[6rem] max-sm:h-[2.25rem] w-[121px] h-[57px]   font-semibold 
+        bg-gradient-to-r   from-[#FDBB57] from-[-60%] 
       rounded-[10px] drop-shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] shadow-[4px_4px_4px_0px_rgba(242,208,155,1)]">Login</button>
         
-        <button className="font-semibold text-[0.8rem]    
-        w-[60px] h-[25px]  bg-gradient-to-r   from-[#FDBB57] from-[-60%] 
+        <button className="font-semibold max-sm:text-[0.9rem]    
+        max-sm:w-[6rem] max-sm:h-[2.25rem]  w-[121px] h-[57px] bg-gradient-to-r   from-[#FDBB57] from-[-60%] 
       rounded-[10px] drop-shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] shadow-[4px_4px_4px_0px_rgba(242,208,155,1)]">Sign up</button>
         </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,24 @@ module.exports = {
         'poppins':'Poppins, sans-serif',
          'frank_ruhi':'Frank Ruhl Libre, serif',
          'Podkova':'Podkova, serif'
-      }
+      },
+      screens: {
+        'md_lg':'1160px',
+        'lg_1300':'1300',
+        '2xl': '1440px',
+        'xsm' : '360px',
+        '916':'920px',         
+        
+        'max-md_lg': {'max': '1160px'},
+
+        'max-xsm': {'max': '360px'},
+        'max-sm':  {'max': '640px'}, 
+        'max-md': {'max': '748px'},
+        'max-lg': {'max': '1024px'},
+        'max-xl': {'max': '1280px'},
+        'max-2xl': {'max': '1440px'},
+        'max-916':{'max':'920px'},   
+      },
     },
   },
   plugins: [],
