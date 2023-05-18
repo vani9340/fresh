dd35db3d3f1987000b2fb148c6c2e7adc90a4d7a
vani9340 <sg5428702@gmail.com>
2023-05-19 00:50:57 +0530

    c

--- src/components/Part5.js
+++ src/components/Part5.js
@@ -6,6 +6,7 @@ import img5 from './images5/5.svg'
 import img6 from './images5/6.svg'
 import img7 from './images5/7.svg'
 import img8 from './images5/8.svg'
+import stars from './images5/stars.svg'
 import { BrowserRouter } from 'react-router-dom'
 import {HashLink as Link} from 'react-router-hash-link'
 
@@ -23,42 +24,120 @@ function Part5(){
     </div>
 
     <div>
+    {/* img part 1 */}
         <div className='flex'>
-            <div className="min-h-[526px] max-w-[318px]">
-                <img src={img1}/>
-                <div></div>
-            </div>
-            <div className="min-h-[526px] max-w-[318px]">
-                <img src={img2}/>
-                <div></div>
-            </div>
-            <div className="min-h-[526px] max-w-[318px]">
-                <img src={img3}/>
-                <div></div>
-            </div>
-            <div className="min-h-[526px] max-w-[318px]">
-                <img src={img4}/>
-                <div></div>
-            </div>
+{/* img1 */}
+<div className="min-h-[551px] max-w-[366px]">
+<img src={img1}/>
+<div class='bg-white'>
+  <img src={stars}/>
+  <h1>Sweater Shirt</h1>
+  <div>
+    <h1>$45.99</h1>
+    <h1>$35.99</h1>
+  </div>
+</div>
+</div>  
+
+{/* img2 */}
+<div className="min-h-[551px] max-w-[366px]">
+<img src={img2}/>
+<div class='bg-white'>
+  <img src={stars}/>
+  <h1>Pastel Long Sleeve</h1>
+  <div>
+    <h1>$55</h1>
+    <h1>$44.99</h1>
+  </div>
+</div>
+</div>  
+
+{/* img3 */}
+<div className="min-h-[551px] max-w-[366px]">
+<img src={img3}/>
+<div class='bg-white'>
+  <img src={stars}/>
+  <h1>T-Shirt orange</h1>
+  <div class='flex'>
+    <h1>$29.99</h1>
+    <h1>$15.99</h1>
+  </div>
+</div>
+</div>  
+
+{/* img4 */}
+<div className="min-h-[551px] max-w-[366px]">
+<img src={img4}/>
+<div class='bg-white'>
+  <img src={stars}/>
+  <h1>Blue Bombers</h1>
+  <div>
+    <h1>$69.99</h1>
+    <h1>$49.99</h1>
+  </div>
+</div>
+</div>  
+
+
         </div>
 
+        
+        {/* images 2nd part */}
         <div className='flex'>
-        <div className="min-h-[526px] max-w-[318px]">
-                <img src={img5}/>
-                <div></div>
-            </div>
-            <div className="min-h-[526px] max-w-[318px]">
-                <img src={img6}/>
-                <div></div>
-            </div>
-            <div className="min-h-[526px] max-w-[318px]">
-                <img src={img7}/>
-                <div></div>
-            </div>
-            <div className="min-h-[526px] max-w-[318px]">
-                <img src={img8}/>
-                <div></div>
-            </div>
+{/* img-5 */}
+<div className="min-h-[551px] max-w-[366px]">
+<img src={img5}/>
+<div class='bg-white'>
+  <img src={stars}/>
+  <h1>Jeans Jacket</h1>
+  <div>
+    <h1>$125</h1>
+    <h1>$99.99</h1>
+  </div>
+</div>
+</div>  
+
+{/* img-6 */}
+<div className="min-h-[551px] max-w-[366px]">
+<img src={img6}/>
+<div class='bg-white'>
+  <img src={stars}/>
+  <h1>Denim shirt</h1>
+  <div>
+    <h1>$100</h1>
+    <h1>$74.99</h1>
+  </div>
+</div>
+</div>  
+
+{/* img-7 */}
+<div className="min-h-[551px] max-w-[366px]">
+<img src={img7}/>
+<div class='bg-white'>
+  <img src={stars}/>
+  <h1>Red Suede sweater</h1>
+  <div>
+    <h1>$29.99</h1>
+    <h1>$19.99</h1>
+  </div>
+</div>
+</div>        
+
+{/* img-8 */}
+<div className="min-h-[551px] max-w-[366px]">
+<img src={img8}/>
+<div class='bg-white'>
+  <img src={stars}/>
+  <h1 class=''>Leather Brown Jacket</h1>
+  <div class='flex'>
+    <h1 class='text-[#C4C4C4] text-[1.5rem] leading-[2.25rem] font-normal'>$90.65</h1>
+    <h1 class='text-[1.5rem] leading-[2.25rem] font-normal'>$79.99</h1>
+  </div>
+</div>
+</div>  
+
+
+
         </div>
     </div>
 </div>
