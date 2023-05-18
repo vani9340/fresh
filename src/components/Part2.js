9f4a025857f6c887f525eb8ba248049e443f35ea
vani9340 <sg5428702@gmail.com>
2023-05-18 20:59:36 +0530

    c

--- src/components/Part2.js
+++ src/components/Part2.js
@@ -5,36 +5,38 @@ import shoes from './images/shoes.svg'
 function Part2(){
     return(
         <>
-<div>
-<div>
-    <h1>
+<div class='mt-[93px] leading-[2.25rem] text-[1.5rem] pl-[6.25vw] pr-[5.13vw]'>
+<div class='text-center'>
+    <h1 class='font-frank_ruhi text-[4rem] leading-[5.16375rem] font-bold '>
     New Collection
     </h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <p class='font-normal text-[1.5rem]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
     <div>
 
-    <div class='flex'>
+    <div class='flex justify-between mt-[40px]'>
+
     <div class="relative">
   <img src={sweater} alt="Image" class="w-full"/>
-  <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-    <div class="text-black text-2xl bg-white">Sweater</div>
+  <div class="w-full absolute bottom-0 left-0 flex text-center pb-[3.08541vw]">
+    <div class="text-black text-[1.5rem] font-normal leading-[2.25rem] bg-white w-full rounded-[20px] ml-[1.4583vw] mr-[0.8971vw] pt-[0.66388vw] pb-[0.456vw]">Sweater</div>
   </div>
 </div>
 
 <div class="relative">
   <img src={jeans} alt="Image" class="w-full"/>
-  <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-    <div class="text-black text-2xl bg-white">jeans</div>
+  <div class="w-full absolute bottom-0 left-0 flex text-center pb-[3.08541vw]">
+    <div class="text-black text-[1.5rem] leading-[2.25rem] font-normal bg-white w-full rounded-[20px] py-[0.625vw]">Jeans</div>
   </div>
 </div>
 
 <div class="relative">
   <img src={shoes} alt="Image" class="w-full"/>
-  <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-    <div class="text-black text-2xl bg-white">basket</div>
+  <div class="w-full absolute bottom-0 left-0 flex text-center pb-[3.08541vw]">
+    <div class="text-black text-[1.5rem] font-normal leading-[2.25rem] bg-white w-full  rounded-[20px] ml-[1.388vw] mr-[1.736vw] pt-[0.66388vw] pb-[0.456vw]">Baskets</div>
   </div>
 </div>
+
    </div>
 
         
--- src/components/Part3.js
+++ src/components/Part3.js
@@ -2,26 +2,39 @@ import best from './images/best_fashion.svg'
 
 function Part3(){
     return(
-<div class='flex'>
+<div class='flex mt-[236px] pl-[3.472vw] pr-[4.166vw]'>
+<div>
+<img src={best}/>
+</div>
 <div>
     
 </div>
-<img src={best}/>
 
-<div>
-    <h1>
+
+<div class='flex flex-col justify-center items-center'>
+    <h1 class='font-frank_ruhi font-bold text-[4rem] leading-[5.16375rem] max-w-[437px] min-h-[162px]'>
     Best Fashion
 Since 2010
     </h1>
-    <p>
+    <p class='font-semibold text-[1.5rem] leading-[2.25rem] max-w-[443px] min-h-[186px] mt-[63px]'>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu, odio pellentesque mattis pulvinar felis. At arcu ornare rhoncus, elementum non viverra. 
     </p>
-    <div class='max-w-[49.5625rem] border-black border-[2px] flex'>
-        <div class='w-1/3'>
-        2010
-        </div>
-        <div class='w-1/3'>5000+</div>
-        <div class='w-1/3'>4500+</div>
+
+    <div class='mt-[132px] w-[793px] min-h-[280px]  border-[2px] flex items-center justify-between bg-gradient-to-r from-[#CD8922] to-[120%] rounded-[20px]  drop-shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)] shadow-[4px_4px_4px_0px_rgba(0, 0, 0, 0.25)]'>
+        
+        <div class='border-r-[50%] border-r-[1px] border-black pr-[1.875vw] pl-[5.3472vw]'>
+        <h1 class='font-Podkova text-[4rem] font-bold leading-[71px]'>2010</h1>
+        <p class='font-normal text-[1.75rem] leading-[2.625rem] pt-[1.944vw]'>Founded</p></div>
+    
+
+        
+        <div class='border-r-[50%] border-r-[1px] border-black pr-[3.055vw]'>
+        <h1 class='font-Podkova text-[4rem] font-bold leading-[71px]'>5000+</h1>
+        <p class='font-normal text-[1.75rem] leading-[2.625rem] pt-[1.944vw]'>Product Sold</p></div>
+       
+        <div class='pr-[2.91vw]'>
+        <h1 class='font-Podkova text-[4rem] font-bold leading-[71px]'>4500+</h1>
+        <p class='font-normal text-[1.75rem] leading-[2.625rem] pt-[1.944vw]'>Best Reviews</p></div>
     </div>
 </div>
 </div>
